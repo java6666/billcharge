da709b61cbefef5b2057b190cc020255de218bf5
王老师 <591674141@qq.com>
2017-04-05 14:48:57 +0800

    添加基础文件

---
+++ src/main/java/com/demo/billcharge/model/dao/TestDao.java
@@ -0,0 +1,8 @@
+package com.demo.billcharge.model.dao;
+
+/**
+ * Created by Administrator on 2017/4/5.
+ * @author  wang
+ */
+public interface TestDao {
+}
---
+++ src/main/java/com/demo/billcharge/model/service/TestService.java
@@ -0,0 +1,8 @@
+package com.demo.billcharge.model.service;
+
+/**
+ * Created by Administrator on 2017/4/5.
+ * @author  wang
+ */
+public class TestService {
+}
